6c011e00be55e557f3663f5608c9906d7f7e3974
robin <robinbabu2000@gmail.com>
2025-02-21 23:46:20 +0530

    project upload

--- src/main/java/com/ty/controller/CurrencyController.java
+++ src/main/java/com/ty/controller/CurrencyController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import com.ty.model.CurrencyConversionRequest;
 import com.ty.service.CurrencyService;
 
 @RestController
+@RequestMapping("/api")
 public class CurrencyController {
 	private final CurrencyService currencyService;
 
